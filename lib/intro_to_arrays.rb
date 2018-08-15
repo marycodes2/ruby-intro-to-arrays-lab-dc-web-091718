6fe8a1a4254152b3d64b40fe72a2fe42ec628b99
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-08-15 20:53:06 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,9 +19,11 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  array.push
+  array.shift
 end
 
-
+def last_element_with_array_methods(array)
+  array.pop
+end
 
   
